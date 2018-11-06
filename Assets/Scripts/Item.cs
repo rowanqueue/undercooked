815b9f066373fed11e67497a0f8b7bc42d4566b6
Teague Stover <teaguefcs@gmail.com>
2018-11-06 16:06:15 -0500

    First Commit

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public int type;//0:nonfood,1:food to be chopped,2:food to be cooked,3:food to be boiled,4:food to be combined,5:base
+                    //0 is pan, pot, fire extinguisher
+                    //1 is raw lettuce, meat, tomatoes
+                    //2 is chopped meat
+                    //4 is cooked meat, chopped lettuce/meat/tomatoes
+                    //5 is bun, plate
+                    //recipe: pbmlt
+    public string name;
+
+    public Item(int t, string n)
+    {
+        type = t;
+        name = n;
+    }
+
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool Equals(Item i)
+    {
+        return i.name.Equals(name) && i.type == type;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//use: moves player
+//loc: on a player
+public class Movement : MonoBehaviour {
+    public string xAxis;
+    public string yAxis; //will actually be z movement bc x,y,z is TRASH!!!!!
+	public float maxSpeed;
+	public float accelerationFactor;
+
+	private Vector3 inputVector;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputVector = HandleInput();
+		//changes where the player is looking
+		if (inputVector != Vector3.zero)
+		{
+            //transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward,inputVector,accelerationFactor*Time.deltaTime,0f));
+            transform.LookAt(transform.position + inputVector);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (inputVector != Vector3.zero)//input?? amazing
+		{
+			rb.AddForce(inputVector*accelerationFactor,ForceMode.Force);//this moves the player
+
+		}
+		else
+		{
+			rb.velocity = new Vector3(0,rb.velocity.y,0);
+		}
+		rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+	}
+
+	Vector3 HandleInput()
+	{
+		float x = Input.GetAxis(xAxis);
+		float y = Input.GetAxis(yAxis);
+		return new Vector3(x, 0, y);
+	}
+}
